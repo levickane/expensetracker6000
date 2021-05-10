4ae03429612709b80412d147273ae5a5f6a646e5
Levi Kane <oneleggedlevi09@gmail.com>
2021-05-10 14:15:08 -0500

    changed the connection file

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,21 @@
-const mysql = require('mysql');
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: PORT,
-    user: 'root',
-    password: 'root',
-    database: 'expensetracker_db',
-  });
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
