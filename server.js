860aba94faae820a78cbf600f8109346a69d5dd2
Zachary <taylor67469@yahoo.com>
2021-05-11 11:04:19 -0500

    fixed sess

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-
+const session = require('express-session');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
