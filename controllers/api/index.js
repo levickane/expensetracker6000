ce6a8895ec7b9bf5cb1da30a311a8f6d31fb3571
Zachary <taylor67469@yahoo.com>
2021-05-11 11:20:05 -0500

    monthname fixed

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const monthName = require('./monthName');
 
-router.use('/users', monthName);
+router.use('/monthname', monthName);
 router.use('/users', userRoutes);
 module.exports = router;
